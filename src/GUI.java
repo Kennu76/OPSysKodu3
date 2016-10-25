e33a834c965fe6c388f29978c36dce70c6b4b5e0
alk_ <ott.oopkaup@gmail.com>
2016-10-25 15:40:50 +0300

    Added buttons to UI to select algorithms

--- src/GUI.java
+++ src/GUI.java
@@ -7,9 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -22,13 +20,17 @@ public class GUI extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
+		VBox vBox = new VBox();
+		vBox.setPadding(new Insets(0,10,0,10));
 
 		GridPane grid = new GridPane();
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(0, 10, 0, 10));
-		grid.getColumnConstraints().add(new ColumnConstraints(0)); // column 1 is 100 wide
-		grid.getColumnConstraints().add(new ColumnConstraints(110)); // column 1 is 100 wide
+		//grid.getColumnConstraints().add(new ColumnConstraints(0));
+		grid.getColumnConstraints().add(new ColumnConstraints(110));
+
+		vBox.getChildren().add(grid);
 
 		Text t = new Text("Vali või sisesta kuni 10 kohaline järjend");
 		Text btnText1 = new Text("Situ pihku prill");
@@ -56,20 +58,32 @@ public class GUI extends Application {
 		btn3.setText("Kolmas");
 		btn4.setText("Enda oma");
 
-		grid.add(t,1,1);
-		grid.add(btn,1,2);
-		grid.add(btn2,1,3);
-		grid.add(btn3,1,4);
-		grid.add(btn4,1,5);
+		grid.add(t,0,1);
+		grid.add(btn,0,2);
+		grid.add(btn2,0,3);
+		grid.add(btn3,0,4);
+		grid.add(btn4,0,5);
 
-		grid.add(btnText1,2,2);
-		grid.add(btnText2,2,3);
-		grid.add(btnText3,2,4);
-		grid.add(btnText4,2,5);
+		grid.add(btnText1,1,2);
+		grid.add(btnText2,1,3);
+		grid.add(btnText3,1,4);
+		grid.add(btnText4,1,5);
 
 
+		HBox hBox = new HBox();
+		hBox.setPadding(new Insets(10,10,10,10));
+		hBox.setSpacing(10);
+		vBox.getChildren().add(hBox);
 
-		Scene scene = new Scene(grid, 300, 250);
+		Button algo1 = new Button("First-Fit");
+		Button algo2 = new Button("Worst-Fit");
+		Button algo3 = new Button("Best-Fit");
+		Button algo4 = new Button("Random-Fit");
+		Button puhasta = new Button("Puhasta väljund");
+
+		hBox.getChildren().addAll(algo1,algo2,algo3,algo4,puhasta);
+
+		Scene scene = new Scene(vBox, 300, 100);
 
 		primaryStage.setTitle("Kodutöö2");
 		primaryStage.setScene(scene);
